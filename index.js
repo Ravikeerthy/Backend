f850ad20325ad9b8e5da65a8d650677dbaddf3f6
Ravikeerthy <ravikeerthy1807@gmail.com>
2025-04-24 17:48:01 +0400

    error message

--- index.js
+++ index.js
@@ -7,50 +7,49 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
-app.use(cors({
-  origin: "https://keerthy-portfolio-work.netlify.app",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://keerthy-portfolio-work.netlify.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.post('/contact', async(req, res) =>{
-    const {name, email, message} = req.body;
+app.post("/contact", async (req, res) => {
+  const { name, email, message } = req.body;
 
-    if(!name || !email || !message){
-       return res.status(400).json({message:"All fields are required"})
-    }
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS
-        }
-      });
-    
-      const mailOptions = {
-        from: email,
-        to: process.env.EMAIL_RECEIVER,
-        subject: `Message from ${name}`,
-        html: `
+  if (!name || !email || !message) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_RECEIVER,
+    subject: `Message from ${name}`,
+    html: `
         <h3>New Message from Portfolio</h3>
         <p><strong>Name:</strong> ${name}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Message:</strong><br>${message}</p>
-      `
-      };
+      `,
+  };
 
-      try {
-        
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({message: "Message sent successfully"});
-
-      } catch (error) {
-
-        console.error('Email sending error:', error);
-    res.status(500).json({ error: 'Failed to send message' });
-      }
-})
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    console.error("Email sending error:", error);
+    res.status(500).json({ error: "Failed to send message",  details: error.message });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  console.log(`Server running on http://localhost:${PORT}`);
+});
